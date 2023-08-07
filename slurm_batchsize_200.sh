a01595a4263ba614e070c92c3ac5a6078b0dfcb0
zia <zia.badar@campus.tu-berlin.de>
2023-08-08 00:43:07 +0500

    changed slurm gpu

--- slurm_batchsize_200.sh
+++ slurm_batchsize_200.sh
@@ -7,9 +7,9 @@
 #SBATCH --time=100:00:00
 #SBATCH --partition=gpu
 # #SBATCH --gres=gpu:a100:2
+#SBATCH --gres=gpu:a100:1
 # #SBATCH --gres=gpu:a100:1
-# #SBATCH --gres=gpu:a100:1
-#SBATCH --gres=gpu:tesla:1
+# #SBATCH --gres=gpu:tesla:1
 # #SBATCH --mem=256G
 #SBATCH --mail-user==zia.badar@campus.tu-berlin.de
 python main_vicreg.py --batch-size 200 --validation-mode
