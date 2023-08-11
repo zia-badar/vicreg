cd5ab61bc7a5e383f269f939b303ee504e71da42
zia <zia.badar@campus.tu-berlin.de>
2023-08-11 12:03:51 +0500

    added encoding dim slurm scripts

---
+++ slurm_encoding_30.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# #SBATCH --account=rsim
+#SBATCH --ntasks=20
+#SBATCH --cpus-per-task=1
+# #SBATCH --mem-per-cpu=12G
+#SBATCH --output=output_encoding_30.log
+#SBATCH --time=100:00:00
+#SBATCH --partition=gpu
+# #SBATCH --gres=gpu:a100:2
+#SBATCH --gres=gpu:a100:1
+# #SBATCH --gres=gpu:a100:1
+# #SBATCH --gres=gpu:tesla:1
+# #SBATCH --mem=256G
+#SBATCH --mail-user==zia.badar@campus.tu-berlin.de
+python main_vicreg.py --batch-size 150 --encodingdim 30 --validation-mode
